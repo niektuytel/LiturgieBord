78ee65c774700ab9c25b4464f2ca3e6c029ba3a7
niektuytel <46424968+niektuytel@users.noreply.github.com>
2025-06-05 22:16:17 +0200

    Correct version
    up-t-date ready for demo

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace LiturgieBord.Pages;
+
+public class IndexModel : PageModel
+{
+    public string InitialDataJson { get; set; } = "[]";
+    private readonly string filePath = Path.Combine(Directory.GetCurrentDirectory(), "sections.json");
+
+    public async Task OnGetAsync()
+    {
+        if (System.IO.File.Exists(filePath))
+        {
+            InitialDataJson = await System.IO.File.ReadAllTextAsync(filePath);
+        }
+    }
+
+    public async Task<IActionResult> OnPostSaveAsync([FromBody] JsonElement content)
+    {
+        var sections = content.GetProperty("sections").ToString();
+        await System.IO.File.WriteAllTextAsync(filePath, sections);
+
+        var generatedHtml = content.GetProperty("generated_html");
+        return await GenerateHtmlAsync(generatedHtml);
+    }
+
+    private static async Task<IActionResult> GenerateHtmlAsync(JsonElement generatedHtml)
+    {
+        string liturgieFilePath = Path.Combine(Environment.CurrentDirectory, "Content", "liturgie_bord_template.html");
+
+        if (!System.IO.File.Exists(liturgieFilePath))
+        {
+            return new JsonResult(new { success = false, message = "Liturgie template file not found." });
+        }
+
+        string fileContent = await System.IO.File.ReadAllTextAsync(liturgieFilePath);
+        string updatedContent = fileContent.Replace("<p>{BORD}</p>", generatedHtml.ToString());
+
+        string outputFilePath = Path.Combine(Path.GetDirectoryName(liturgieFilePath)!, "liturgie_bord_generated.html");
+        await System.IO.File.WriteAllTextAsync(outputFilePath, updatedContent);
+
+        return new JsonResult(new { success = true });
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+namespace LiturgieBord
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorPages();
+            builder.Services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
